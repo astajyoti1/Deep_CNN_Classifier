c90e895bd0b6a753ff49ba65cfecbad703211ac3
astajyoti1 <astajyoti@gmail.com>
2022-10-03 12:54:17 +0530

    added tests

--- template.py
+++ template.py
@@ -2,31 +2,33 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s: ')
 
-project_name = "DeepClassifier"
+package_name = "DeepClassifier"
 
 list_of_files = [
-    ".github/workflows/.gitkeep",
-    f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
-    f"src/{project_name}/utils/__init__.py",
-    f"src/{project_name}/config/__init__.py",
-    f"src/{project_name}/pipeline/__init__.py",
-    f"src/{project_name}/entity/__init__.py",
-    f"src/{project_name}/constants/__init__.py",
-    "configs/config.yaml",
-    "dvc.yaml",
-    "params.yaml",
-    "init_setup.sh",
-    "requirements.txt",
-    "requirements_dev.txt",
-    "setup.py",
-    "setup.cfg",
-    "pyproject.toml",
-    "tox.ini",
-    "research/trials.ipynb",
-
+   ".github/workflows/.gitkeep",
+   f"src/{package_name}/__init__.py", 
+   f"src/{package_name}/components/__init__.py", 
+   f"src/{package_name}/utils/__init__.py", 
+   f"src/{package_name}/config/__init__.py", 
+   f"src/{package_name}/pipeline/__init__.py", 
+   f"src/{package_name}/entity/__init__.py", 
+   f"src/{package_name}/constants/__init__.py",
+   "tests/__init__.py",
+   "tests/unit/__init__.py",
+   "tests/integration/__init__.py",
+   "configs/config.yaml",
+   "dvc.yaml",
+   "params.yaml",
+   "init_setup.sh",
+   "requirements.txt", 
+   "requirements_dev.txt",
+   "setup.py",
+   "setup.cfg",
+   "pyproject.toml",
+   "tox.ini",
+   "research/trials.ipynb", 
 ]
 
 for filepath in list_of_files:
@@ -34,10 +36,12 @@ for filepath in list_of_files:
     filedir, filename = os.path.split(filepath)
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Created directory: {filedir} for file: {filename}")
-    if (not os.path.exists(filepath)) or (os.filesize(filepath) == 0):
-        with open(filepath, 'w') as f:
-            pass # Create empty file
-        logging.info(f"Created empty file: {filepath}")
+        logging.info(f"Creating directory: {filedir} for file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass # create an empty file
+            logging.info(f"Creating empty file: {filepath}")
+
     else:
-        logging.info(f"File: {filepath} already exists")
+        logging.info(f"{filename} already exists")
